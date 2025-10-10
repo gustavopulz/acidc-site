fd5a74dd452842bec113b945279e636a15fdd839
gustavopulz <pulzgustavo1@gmail.com>
2025-10-10 16:33:53 -0300

    Implement code changes to enhance functionality and improve performance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,10 +30,7 @@ export default function Home() {
         <div className="grid gap-4 sm:grid-cols-2">
           {/* use arquivos locais em /public ou URLs externas */}
           <AudioPlayer src="/back-in-black.mp3" title="Back In Black" />
-          <AudioPlayer
-            src="/highway-to-hell-sample.mp3"
-            title="Highway To Hell"
-          />
+          <AudioPlayer src="/highway-to-hell.mp3" title="Highway To Hell" />
         </div>
       </Section>
 
