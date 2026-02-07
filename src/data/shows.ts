c6f35d9f418870e4e937fe78d378a156d2a311e6
gustavopulz <pulzgustavo1@gmail.com>
2026-02-07 12:01:04 -0300

    feat: update shows data with new event details and remove outdated entries

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -1,32 +1,32 @@
 import type { ShowInfo } from "../components/ShowCard";
 
 export const shows: ShowInfo[] = [
-  // {
-  //   date: "18/10/2025",
-  //   city: "Itapira - SP",
-  //   venue: "Country Club - 17h",
-  //   status: "soldout",
-  //   link: "https://www.instagram.com/country_club_itapira",
-  // },
-  // {
-  //   date: "15/11/2025",
-  //   city: "Rio Claro - SP",
-  //   venue: "Aniversário do Zapata MC",
-  //   status: "soon",
-  //   link: "https://www.instagram.com/zapata_subsede_rioclaro",
-  // },
-  // {
-  //   date: "22/11/2025",
-  //   city: "Cali Skate Bar",
-  //   venue: "Mogi Mirim - SP",
-  //   status: "tickets",
-  //   link: "https://www.instagram.com/cali_skate_company",
-  // },
   {
-    date: "--/--/----",
-    city: "Agenda para 2026 aberta!",
-    venue: "Contate-nos para reservar sua data.",
+    date: "11/04/2026",
+    city: "Leme - SP",
+    venue: "Independente MC",
     status: "soon",
-    link: "https://www.instagram.com/cafepubleme",
+    link: "https://www.instagram.com/country_club_itapira",
   },
+  {
+    date: "20/06/2026",
+    city: "Leme - SP",
+    venue: "Kaiowas MC",
+    status: "soon",
+    link: "https://www.instagram.com/zapata_subsede_rioclaro",
+  },
+  {
+    date: "28/11/2026",
+    city: "Ibitinga - SP",
+    venue: "Alta Voltagem MC",
+    status: "tickets",
+    link: "https://www.instagram.com/cali_skate_company",
+  },
+  // {
+  //   date: "--/--/----",
+  //   city: "Agenda para 2026 aberta!",
+  //   venue: "Contate-nos para reservar sua data.",
+  //   status: "soon",
+  //   link: "https://www.instagram.com/cafepubleme",
+  // },
 ];
